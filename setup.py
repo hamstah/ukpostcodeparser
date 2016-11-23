2f521d8c723e7dc5c38d1c76d5c739a54f8c21d3
Luis Visintini <lvisintini@gmail.com>
2016-11-23 10:56:12 +0000

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='UkPostcodeParser',
-    version='1.0.3',
+    version='1.1.0',
     author='Simon Brunning',
     author_email='simon@brunningonline.net',
     packages=['ukpostcodeparser', 'ukpostcodeparser.test'],
